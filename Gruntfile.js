720ab59ee554e453bf745f16a35616519ed3375d
Александра Простякова < >
2017-10-22 13:18:36 +0400

    grunt svg

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,13 @@
 "use strict";
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-less");
   grunt.loadNpmTasks("grunt-browser-sync");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-postcss");
-
+  grunt.loadNpmTasks('grunt-svg-sprites');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.initConfig({
     less: {
       style: {
@@ -45,13 +47,42 @@ module.exports = function(grunt) {
       }
     },
 
+    svg_sprites: {
+      options: {
+        // Task-specific options go here.
+      },
+      your_target: {
+        src: 'img/svg/*.svg',
+        dest: 'img/spritesheet'
+      }
+    },
     watch: {
       style: {
         files: ["less/**/*.less"],
         tasks: ["less", "postcss"]
       }
+    },
+
+    uglify: {
+      build: {
+        src: 'js/build/production.js',
+        dest: 'js/build/production.min.js',
+      }
+    },
+
+    concat: {
+
+      dist: {
+        src: [
+          'js/libs/*.js', // All JS in the libs folder
+          'js/controls.js', // This specific file
+        ],
+        dest: 'dist/built.js',
+      }
     }
   });
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
+  grunt.registerTask('default', ["uglify"]);
+  grunt.registerTask('default', ['concat', 'uglify']);
 };
