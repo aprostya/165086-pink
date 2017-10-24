571246c22946ff32f782eb55510dc1b7fe8f92a3
Александра Простякова < >
2017-10-24 18:09:29 +0400

    svg addings

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,82 +5,90 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-browser-sync");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-postcss");
-  grunt.loadNpmTasks('grunt-svg-sprites');
+  grunt.loadNpmTasks('grunt-svg-sprite');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.initConfig({
-    less: {
-      style: {
-        files: {
-          "css/style.css": "less/style.less"
+      less: {
+        style: {
+          files: {
+            "css/style.css": "less/style.less"
+          }
         }
-      }
-    },
-
-    postcss: {
-      style: {
-        options: {
-          processors: [
-            require("autoprefixer")()
-          ]
-        },
-        src: "css/*.css"
-      }
-    },
-
-    browserSync: {
-      server: {
-        bsFiles: {
-          src: [
-            "*.html",
-            "css/*.css"
-          ]
-        },
-        options: {
-          server: ".",
-          watchTask: true,
-          notify: false,
-          open: true,
-          cors: true,
-          ui: false
-        }
-      }
-    },
-
-    svg_sprites: {
-      options: {
-        // Task-specific options go here.
       },
-      your_target: {
-        src: 'img/svg/*.svg',
-        dest: 'img/spritesheet'
-      }
-    },
-    watch: {
-      style: {
-        files: ["less/**/*.less"],
-        tasks: ["less", "postcss"]
-      }
-    },
 
-    uglify: {
-      build: {
-        src: 'js/build/production.js',
-        dest: 'js/build/production.min.js',
-      }
-    },
+      postcss: {
+        style: {
+          options: {
+            processors: [
+              require("autoprefixer")()
+            ]
+          },
+          src: "css/*.css"
+        }
+      },
 
-    concat: {
+      browserSync: {
+        server: {
+          bsFiles: {
+            src: [
+              "*.html",
+              "css/*.css"
+            ]
+          },
+          options: {
+            server: ".",
+            watchTask: true,
+            notify: false,
+            open: true,
+            cors: true,
+            ui: false
+          }
+        }
+      },
+      svg_sprite: [{
+        expand: true,
+        src: ['img/svg/*.svg'],
+        dest: 'img/spritesheet',
 
-      dist: {
-        src: [
-          'js/libs/*.js', // All JS in the libs folder
-          'js/controls.js', // This specific file
-        ],
-        dest: 'dist/built.js',
+        options: {
+          mode: {
+            inline: true,
+            symbol: true,
+            css: false
+          }
+        }
+      }],
+
+      watch: {
+        style: {
+          files: ["less/**/*.less"],
+          tasks:
+            ["less", "postcss"]
+        }
+      },
+
+      uglify: {
+        build: {
+          src: 'js/build/production.js',
+          dest:
+            'js/build/production.min.js',
+        }
+      },
+
+      concat: {
+
+        dist: {
+          src: [
+            'js/libs/*.js', // All JS in the libs folder
+            'js/controls.js', // This specific file
+          ],
+          dest:
+            'dist/built.js',
+        }
       }
     }
-  });
+  );
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
   grunt.registerTask('default', ["uglify"]);
